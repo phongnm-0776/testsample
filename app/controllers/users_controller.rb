8311b9763fb199b84aaebcd2351064273a0b1b15
Phong <ngo.minh.phong@framgia.com>
2018-12-17 00:57:14 +0700

    1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,80 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:show, :new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :load_user, except: [:index, :new, :create]
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.where(activated: true).paginate page: params[:page],
+      per_page: Settings.users.index.per_page
+  end
+
+  def show
+    redirect_to signup_path unless @user.present? || @user.activated
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      @user.send_activation_email params[:locale]
+      flash[:info] = t "flash.activation_mail"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    redirect_to login_path if @user.nil?
+  end
+
+  def update
+    # load_user
+    if @user.update_attributes(user_params) && @user.present?
+      flash[:success] = t "flash.updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "flash.deleted"
+    elsif @user.nil?
+      flash[:danger] = t "flash.delete_fail"
+    end
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
+  end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "flash.notlogin"
+    redirect_to login_path
+  end
+
+  def correct_user
+    load_user
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    flash[:danger] = t "flash.nouser" if @user.nil?
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,84 @@
+class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+  has_secure_password
+  validates :password, presence: true,
+  length: {minimum: Settings.users.password.min_length}, allow_nil: true
+  before_save{email.downcase!}
+  validates :name, presence: true,
+  length: {maximum: Settings.users.name.max_length}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+  length: {maximum: Settings.users.name.max_length},
+  format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+  # Returns the hash digest of the given string.
+  def self.digest string
+    cost =
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  # Activates an account.
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  # Sends activation email.
+  def send_activation_email locale
+    UserMailer.account_activation(self, locale).deliver_now
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token),
+      reset_sent_at: Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+end
